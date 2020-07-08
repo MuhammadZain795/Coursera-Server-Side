5a06f948fb0f30cdd6be288f3868b677387a9537
MuhammadZain795 <bsef17m030@pucit.edu.pk>
2020-07-08 14:44:18 +0500

    assinment 1

--- NodeJs/Node-Express/index.js
+++ NodeJs/Node-Express/index.js
@@ -1,21 +1,37 @@
-const express = require('express'),
-     http = require('http');
-
-const dishRouter = require('./routes/dishRouter');
-const hostname = 'localhost';
-const bodyParser = require('body-parser');
-const port = 3000;
+const http = require('http');
+const express = require('express');
 const morgan = require('morgan');
+
+const hostname = 'localhost';
+const port = 3000;
+
+const bodyPraser = require('body-parser');
+
 const app = express();
+
+
+app.use(bodyPraser.json());
 app.use(morgan('dev'));
 
+app.use(express.static(__dirname + '/public'));
+
+const dishRouter = require('./routes/dishRouter');
+const leaderRouter = require('./routes/leaderRouter');
+const promoRouter = require('./routes/promoRouter');
+app.use('/leaders',leaderRouter);
 app.use('/dishes', dishRouter);
+app.use('/promotions', promoRouter);
 
-app.use(bodyParser.json());
+app.use((req,res,nest) => {
+    console.log(req.headers);
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<html><body><h1>This is an Express Server</h1></body></html>');
 
+});
 
 const server = http.createServer(app);
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+server.listen(port, hostname, ()=>{
+    console.log(`server running at http://${hostname}:${port}/`);
 });
--- NodeJs/Node-Express/routes/dishRouter.js
+++ NodeJs/Node-Express/routes/dishRouter.js
@@ -25,4 +25,25 @@ dishRouter.route('/')
     res.end('Deleting all dishes');
 });
 
-module.exports = dishRouter;
+dishRouter.route('/:dishId')
+.all((req,res,next) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    next();
+})
+
+.get((req,res,next) => {
+    res.end('Will send the dish:'+req.params.dishId+' to you!');
+})
+.post((req, res, next) => {
+    res.end('POST operation not supported on dish:'+req.params.dishId);
+})
+.put((req, res, next) => {
+    res.statusCode = 403;
+    res.end('\will update the dish:'+req.params.dishId + ' with name:'+req.body.name +' with description : ' + req.body.description);
+})
+.delete((req, res, next) => {
+    res.end('Deleting the dish with id:'+req.params.dishId);
+});
+
+module.exports = dishRouter; 
