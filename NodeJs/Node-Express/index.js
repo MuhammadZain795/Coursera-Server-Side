d88a57051a5620ce3063cd2bd5d1fb53ea4d0836
MuhammadZain795 <bsef17m030@pucit.edu.pk>
2020-07-08 12:00:57 +0500

    Express router

--- NodeJs/Node-Express/index.js
+++ NodeJs/Node-Express/index.js
@@ -1,19 +1,18 @@
 const express = require('express'),
      http = require('http');
 
+const dishRouter = require('./routes/dishRouter');
 const hostname = 'localhost';
+const bodyParser = require('body-parser');
 const port = 3000;
 const morgan = require('morgan');
 const app = express();
 app.use(morgan('dev'));
 
-app.use(express.static(__dirname + '/public'));
-app.use((req, res, next) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<html><body><h1>This is an Express Server</h1></body></html>');
+app.use('/dishes', dishRouter);
 
-});
+app.use(bodyParser.json());
+
 
 const server = http.createServer(app);
 
